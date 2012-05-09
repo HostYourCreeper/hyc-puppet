0ae43a67295fe9efe79942d5c0c30e80f92a3d4c
Emilien Kenler <milk@hostyourcreeper.com>
2012-05-09 09:56:52 +0200

    Module xen-tools
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

---
+++ modules/xen-tools/manifests/init.pp
@@ -0,0 +1,54 @@
+# Class: xen-tools
+#
+# This module manages xen-tools
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class xen-tools {
+    package { 'xen-hypervisor-4.0-amd64':
+      ensure => latest,
+    }
+    package { 'xen-tools':
+      ensure => latest,
+      require => Package['xen-hypervisor-4.0-amd64'],
+    }
+    file {'/etc/xen/xend-config.xsp':
+      ensure => directory,
+      mode   => 0644,
+      require => Package['xen-hypervisor-4.0-amd64'],
+    }
+    file {'/etc/xen-tools/xen-tools.conf':
+      ensure  => directory,
+      mode    => 0644,
+      require => Package['xen-tools'],
+    }
+    file {'/etc/xen-tools/nginx.d':
+      ensure  => directory,
+      mode    => 0644,
+      source  => 'puppet:///xen-tools/nginx.d',
+      recurse => true,
+      require => File['/etc/xen-tools/xen-tools.con'],
+    }
+    file {'/etc/xen-tools/minecraft.d':
+      ensure  => directory,
+      mode    => 0644,
+      source  => 'puppet:///xen-tools/minecraft.d',
+      recurse => true,
+      require => File['/etc/xen-tools/xen-tools.con'],
+    }
+    file {'/etc/xen-tools/role.d':
+      ensure  => directory,
+      mode    => 0644,
+      source  => 'puppet:///xen-tools/role.d',
+      recurse => true,
+      purge   => false,
+      require => File['/etc/xen-tools/xen-tools.con'],
+    }
+}
