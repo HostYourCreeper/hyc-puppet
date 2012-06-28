9a4e16e4251b8f58fc13acc375dad9b021c336fb
Emilien Kenler <milk@hostyourcreeper.com>
2012-06-28 17:20:03 +0200

    Retrait monitoring & puppet des vms
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -30,18 +30,18 @@ node /^server\d{3}\.hyc\d{3}\.hostyourcreeper\.net$/ inherits basenode{
   include ntp
   include murmur
   include backups
-  include puppet::onetime
+  #include puppet::onetime
   include monit
   include minecraft
-  monit::service { "puppet-agent": }
+  #monit::service { "puppet-agent": }
   monit::service { "ntp": }
-  include munin::client
-  munin::plugin { df: }
-  munin::plugin { df_abs: }
-  munin::plugin { netstat: }
-  munin::plugin { processes: }
-  munin::plugin { cpu: }
-  munin::plugin { load: }
-  munin::plugin { memory: }
-  munin::plugin { swap: }
+  #include munin::client
+  #munin::plugin { df: }
+  #munin::plugin { df_abs: }
+  #munin::plugin { netstat: }
+  #munin::plugin { processes: }
+  #munin::plugin { cpu: }
+  #munin::plugin { load: }
+  #munin::plugin { memory: }
+  #munin::plugin { swap: }
 }
