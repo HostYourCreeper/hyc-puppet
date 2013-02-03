2c27f2a2105cc22e790e74362951cd8d0761d38a
Emilien Kenler <milk@hostyourcreeper.com>
2013-02-03 14:14:08 +0100

    fw
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- modules/firewall/files/firewall.sh
+++ modules/firewall/files/firewall.sh
@@ -15,34 +15,34 @@ start(){
  
   iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
  
+  iptables -I INPUT -i lo -j ACCEPT
+  iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+  iptables -A INPUT -d ${IP}/32 -p tcp -m tcp --dport 22 -j ACCEPT
+  iptables -A INPUT -d ${IP}/32 -p tcp -m tcp --dport 8124 -j ACCEPT
+  iptables -A INPUT -d ${IP}/32 -p tcp -m tcp --dport 5666 -j ACCEPT
+
   ## Start Nginx VM ##
   # SSH
-  #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 20122 -j DNAT --to-destination 10.10.10.01:22
   iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 20122 -j DNAT --to-destination 10.10.10.01:22
   # HTTP
-  #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.10.10.01:80
   iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.10.10.01:80
   ## End Nginx VM ##
 
   for i in ${DIR}/vm/* ; do
     NUM=$(echo $i | awk -F\/ '{print $5}')
     # FTP 2XX21 -> 21
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 2${NUM}21 -j DNAT --to-destination 10.10.10.${NUM}:21
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 2${NUM}21 -j DNAT --to-destination 10.10.10.${NUM}:21
 
     # MC 255XX -> 25565
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 255${NUM} -j DNAT --to-destination 10.10.10.${NUM}:25565
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 255${NUM} -j DNAT --to-destination 10.10.10.${NUM}:25565
 
     # Minequery 256XX -> 25566
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 256${NUM} -j DNAT --to-destination 10.10.10.${NUM}:25566
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 256${NUM} -j DNAT --to-destination 10.10.10.${NUM}:25566
    
     # Rcon 275XX -> 25575
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 275${NUM} -j DNAT --to-destination 10.10.10.${NUM}:25575
 
     # HTTP 80XX -> 80
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 80${NUM} -j DNAT --to-destination 10.10.10.${NUM}:80
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 80${NUM} -j DNAT --to-destination 10.10.10.${NUM}:80
     iptables -t nat -A PREROUTING -s 91.236.239.44/32 -d ${IP}/32 -p tcp -m tcp --dport 81${NUM} -j DNAT --to-destination 10.10.10.${NUM}:81
 
@@ -51,17 +51,13 @@ start(){
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 123${NUM} -j DNAT --to-destination 10.10.10.${NUM}:8123
 
     # Mumble 646XX -> 64738
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 646${NUM} -j DNAT --to-destination 10.10.10.${NUM}:64738
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 646${NUM} -j DNAT --to-destination 10.10.10.${NUM}:64738
-    #iptables -t nat -A PREROUTING -i eth0 -p udp -m udp --dport 646${NUM} -j DNAT --to-destination 10.10.10.${NUM}:64738
     iptables -t nat -A PREROUTING -d ${IP}/32 -p udp -m udp --dport 646${NUM} -j DNAT --to-destination 10.10.10.${NUM}:64738
 
     # SSH 2XX22 -> 22
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 2${NUM}22 -j DNAT --to-destination 10.10.10.${NUM}:22
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 2${NUM}22 -j DNAT --to-destination 10.10.10.${NUM}:22
 
     # Munin 149XX -> 4949
-    #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 149${NUM} -j DNAT --to-destination 10.10.10.${NUM}:4949
     iptables -t nat -A PREROUTING -d ${IP}/32 -p tcp -m tcp --dport 149${NUM} -j DNAT --to-destination 10.10.10.${NUM}:4949
 
     if [ -s ${i} ]
@@ -71,10 +67,13 @@ start(){
     fi
 
   done
+
+  iptables -A INPUT -p udp -j DROP
 }
 stop(){
   iptables -F -t nat
   iptables -X -t nat
+  iptables -F INPUT
 }
 
 show(){
@@ -89,8 +88,6 @@ start)
 ;;
 stop)
   stop;
-  stop-realmd;
-  stop-worldd;
   date;
   echo "Firewall stopped";
 ;;
