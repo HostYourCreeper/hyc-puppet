f24b6f945d0f55ccc67034c91fec2fef7a3b9141
Emilien Kenler <milk@hostyourcreeper.com>
2014-05-31 10:46:18 +0900

    Clean before release

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,14 +6,9 @@ node basenode {
 node /^hyc\d{3}\.hostyourcreeper\.net$/ inherits basenode{
   include firewall
   include xen-tools
-  include hyc-api
   include ntp
   include puppet::daemon
   include puppet::node
-  include monit
-  monit::service { "api": }
-  monit::service { "puppet-agent": }
-  monit::service { "ntp": }
   include nagios::nrpe-node
   include nagios::nsca
 }
@@ -26,11 +21,9 @@ node /^nginx.hyc\d{3}\.hostyourcreeper\.net$/ inherits basenode{
 node /^server\d{3}\.hyc\d{3}\.hostyourcreeper\.net$/ inherits basenode{
   include ntp
   include murmur
-  #include backups
+  include backups
   include puppet
   include puppet::onetime
-  include monit
   include minecraft
   include nagios::nrpe-vm
-  monit::service { "ntp": }
 }
